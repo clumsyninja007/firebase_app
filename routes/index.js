7cafc7302dc8c2f9000b561cba40aad2a99c824d
Archer Harmony <aharmony411@gmail.com>
2018-09-27 16:55:30 -0700

    Changed multiparty to busboy and added page which displays the data extracted from CSV and inserted into MySQL

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
-var multiparty = require('multiparty');
+var Busboy = require('busboy');
 var util = require('util');
 
-const csv = require('csv-parse');
+const csv = require('csv-string');
 
 var mysql = require('mysql');
-var async = require('async');
 
 var postResults = [];
 
-/*function getBatch(c, callback) {
-  c.query('SELECT MAX(batch_num) AS batch FROM customers', (err, result) => {
-    if (err) return callback(true, err);
-    console.log("Result: " + result);
-    console.log("New batch: " + batch);
-    return callback(null, Number(result[0].batch) + 1);
-  });
-}*/
+/*
+var sql = `INSERT INTO customers (batch_num, date_userID_created, snn, \
+  gender, first_name, last_name, lic_num, birthdate, \
+  points_strike, dl_class) VALUES (${data[0]}, '${data[1]}', '${data[2]}', \
+  '${data[3]}', '${data[4]}', '${data[5]}', '${data[6]}', \
+  '${data[7]}', '${data[8]}', '${data[9]}')`;
+con.query(sql, function(err, result) {
+if (err) throw err;
+console.log("1 record inserted");
+});
+*/
+
+class Database {
+  constructor(config) {
+    this.connection = mysql.createConnection(config);
+  }
+  query(sql,args) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(sql,args,(err,rows) => {
+        if (err)
+          return reject(err);
+        resolve(rows);
+      });
+    });
+  }
+  close() {
+    return new Promise((resolve, reject) => {
+      this.connection.end(err => {
+        if (err)
+          return reject(err);
+        resolve();
+      });
+    });
+  }
+}
 
 // Home page
 router.get('/', function(req, res, next) {
@@ -32,81 +58,59 @@ router.get('/upload', function(req, res, next) {
 
 // Insert data from uploaded file to MySQL
 router.post('/upload-success', function(req, res, next) {
-  async.series([
-    function(){
-      var con = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "Le0dav!s",
-        database: "firebase_app_sql"
+  var con = new Database({
+    host: "localhost",
+    user: "root",
+    password: "Le0dav!s",
+    database: "firebase_app_sql"
+  });
+
+  var busboy = new Busboy({ headers: req.headers });
+  CSVData = [];
+  busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+    console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+    if (mimetype === 'text/csv') {
+      var buffer = '';
+      file.on('data', function(data) {
+        console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+        buffer += data;
+        //CSVData = csv();
       });
-
-      con.connect();
-    },
-    function(){
-      var form = new multiparty.Form();
-
-      // format date in desired yyyy-mm-dd format
-      //var date = new Date().toISOString()
-      //          .replace(/T.*/, '');
-
-      form.on('error', (err) => {
-        console.log('Error parsing form: ' + err.stack);
-        res.render('index', { title: 'Choose an Option', success: false });
+      file.on('end', function() {
+        console.log('File [' + fieldname + '] Finished');
+        CSVData.push(buffer);
+        //console.log('File: ' + util.inspect(file));
       });
-
-      form.on('part', (part) => {
-        if (!part.filename) {
-          console.log('got field named ' + part.name);
-          part.resume();
-        }
-
-        if (part.filename) {
-          console.log('got file name ' + part.name);
-          
-          // pipe the readstream data to the csv parser
-          part.pipe(csv()).on('data', (data) => {
-            console.log(data);
-
-            postResults.push(data);
-
-            // skip the header row
-            if (data[0] !== 'batch_num') {
-
-              var sql = `INSERT INTO customers (batch_num, date_userID_created, snn, \
-                          gender, first_name, last_name, lic_num, birthdate, \
-                          points_strike, dl_class) VALUES (${data[0]}, '${data[1]}', '${data[2]}', \
-                          '${data[3]}', '${data[4]}', '${data[5]}', '${data[6]}', \
-                          '${data[7]}', '${data[8]}', '${data[9]}')`;
-              con.query(sql, function(err, result) {
-                if (err) throw err;
-                console.log("1 record inserted");
-              });
-
-            }
-
-          });
-          part.resume();
-        }
-
-        part.on('error', (err) => {
-          console.log('part error: ' + err.stack);
-          res.render('index', { title: 'Choose an Option', success: false });
-        });
-      });
-
-      // Close emitted after form parsed
-      form.on('close', function() {
-        console.log('Upload completed!');
-        res.end();
-      });
-
-      form.parse(req);
-    },
-    function(){
-      res.render('upload-success', { title: 'Successfully uploaded file', results: postResults });
     }
-  ]);
+    else {
+      file.on('end', function() {
+        console.log('Non-CSV file uploaded');
+      });
+    }
+  });
+  busboy.on('finish', function() {
+    console.log('Done parsing form!');
+    CSVData = csv.parse(CSVData[0]);
+    console.log('First element of CSV Data: ' + CSVData[0]);
+    //console.log(util.inspect(CSVData[0]));
+    if (CSVData[0][0] === 'batch_num') {
+      CSVData.shift();
+      console.log('Removing header row...');
+    }
+    console.log('Final CSV Data: ' + CSVData);
+
+    CSVData.forEach(data => {
+      con.query(`INSERT INTO customers (batch_num, date_userID_created, snn, \
+        gender, first_name, last_name, lic_num, birthdate, \
+        points_strike, dl_class) VALUES (${data[0]}, '${data[1]}', '${data[2]}', \
+        '${data[3]}', '${data[4]}', '${data[5]}', '${data[6]}', \
+        '${data[7]}', '${data[8]}', '${data[9]}')`);
+      console.log('1 row inserted');
+    });
+
+    res.render('upload-success', { title: 'CSV Uploaded!', results: CSVData });
+  });
+  req.pipe(busboy);
 });
 
 module.exports = router;
